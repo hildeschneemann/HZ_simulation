7928051f77a364af9973188520f330cd70b6014a
hildeschneemann <hfhschneemann@gmail.com>
2018-10-30 12:59:23 +0100

    update everything

--- diploid/clean_version/full_model/recursion_bis_bb.cpp
+++ diploid/clean_version/full_model/recursion_bis_bb.cpp
@@ -18,19 +18,12 @@ extern FILE * fichierE;
 extern FILE * fichierS;
 extern MTRand rnd;
 
-double getfitness(double HI, int p12, int lv, double kd2, double a, double f, double p, double r)
+double getfitness(double d, double kd2, double a)
 {
-	double sz2;
-	double p12prop = double(p12) / double(lv);
-	sz2 = p + (4.0 - 2.0*p)*HI*(1.0-HI)+(f-1.0)*p12prop+(f-1.0+r)*p12prop*(1.0-p12prop);
+	double sz2 = d * d;
 	return(exp(-a * pow(sz2,kd2)));
 }
 
-int getfitness_index(int p2, int p12, int lv)
-{
-	int notp2 = lv - p2;
-	return((notp2 * (notp2+1))/ 2 + p12);
-}
 
 /*----------------------------------------------------------
 
@@ -47,9 +40,9 @@ Lv: genome map length (mean nb of cross-overs per meiosis)
 * FGM parameters
 kv: curvature of fitness function (cf Gros et al 2009 Genetics)
 av: strength of selection (steepness of fitness function)
-fv: fitness breakdown in F1 hybrids
-pv: fitness breakdown in parents
-rv: contribution of heterozygosity to curvature
+nv: number of phenotypic dimensions (complexity)
+plv: number of dimensions affected by a mutation (pleiotropy)
+stv: mutational step sizes along each phenotypic axis
 
 * Simulation parameters
 tv: number of generations that are saved and after which the
@@ -60,14 +53,14 @@ pasv: generation steps
 
 void recursion(	int Dv, int Nv, double mv, 
 				int lv, double Lv,
-				double kv, double av, double fv, double pv, double rv,
+				double kv, double av, int nv, int plv, double stv,
 				int tv, int pasv)
 
 {
 	// variables:
 
 	int i, j, k, loc, gen, par1, par2, ind, nb, nb2, nb3, nb4, nbMig, p2, p12;
-	double w, wbar, varw, d, delta_p, p_old, HI;
+	double w, wbar, d, delta_p, p_old, HI, sz2;
 	bool withrec;
 	vector<int> store;
 
@@ -87,8 +80,6 @@ void recursion(	int Dv, int Nv, double mv,
 	int nbSign = 0;
 	withrec = Lv == -1 ? false : true;
 	int round = 0;	
-	int nbW = (lv + 2) * (lv + 1) / 2.0;
-	int indexW = 0;
 
 
 	boost::dynamic_bitset<> tmp1;
@@ -96,14 +87,12 @@ void recursion(	int Dv, int Nv, double mv,
 	boost::dynamic_bitset<> tmp3;
 	boost::dynamic_bitset<> tmp4;
 
-	
-	// HDF5 constants and variables
 
 	string fileName;
 	stringstream nameF;
 	nameF << "res_D" << Dv << "_N" << Nv << "_m" << mv << 
 		"_l" << lv << "_L" << Lv << 
-		"_k" << kv << "_a" << av << "_f" << fv << "_p" << pv << "_r" << rv <<
+		"_k" << kv << "_a" << av << "_n" << nv << "_pl" << plv << "_st" << stv <<
 		"_t" << tv << ".txt";
 	nameF >> fileName;
 
@@ -112,6 +101,9 @@ void recursion(	int Dv, int Nv, double mv,
     chr * pop = new chr [twoND];
 	chr * temp = new chr [twoND]; // used to generate next generation
     chr * cp;
+
+
+	double * mutations = new double [lv * nv];
 
 	// "Wtot" will hold the fitness of each individual:
 	double * Wtot = new double [ND];
@@ -122,16 +114,10 @@ void recursion(	int Dv, int Nv, double mv,
 	// "HIend" will hold the hybrid index of each individual at the last generation
 	
 	double * HIend = new double [ND];
+
+	double * Brown = new double [lv + 1];
 	
-	// "Wtable" will be the lookup table for the fitness corresponding to a genotype
 	
-	double * Wtable = new double [nbW];
-	
-	// "Wsave" will save the number of individuals with a certain fitness value to output
-	
-	double * Wsave = new double [nbW];
-	
-	// create HDF5 file and open it
 
 	time_t debut, fin;
 	struct tm *ptr;
@@ -152,22 +138,21 @@ void recursion(	int Dv, int Nv, double mv,
     }	
 	
 	
-	// create fitness lookup table
-	for (i = lv; i > -1; i--)
+	for (i = 0; i < (lv * nv); i++)
+		mutations[i] = 0;
+	for (i = 0; i < nv; i++)
 	{
-		p2 = i;
-		for (j=0; j < (lv - i + 1); j++)
-		{	
-			p12 = j;
-			HI = (p2 + p12 / 2.0) / lv;
-			indexW = getfitness_index(p2, p12, lv);
-			Wtable[indexW] = getfitness(HI, p12, lv, kd2, av,  fv,  pv, rv);
-			cout << indexW << "\tp2: " << p2 << "\tp12: " << p12 << "\tHI: " << HI << "\tfitness: " << Wtable[indexW] << "\n";
-			//indexW += 1;
+		nb = lv * i;
+		brownian_bridge(Brown, lv+1);
+		for (j = 0; j < lv; j++)
+		{
+		//	cout << "ok before mut?\n";
+			mutations[nb + j] = stv * (Brown[j+1] - Brown[j]);
+			cout << nb << "\t" << j << "\t" << mutations[nb + j] << "\n";
 		}
 	}
 	
-	
+
 	
    
     // generations:
@@ -189,10 +174,7 @@ void recursion(	int Dv, int Nv, double mv,
 				
 			for (i = 0; i < Dv; i++)
 			{
-				for (j = 0; j < nbW; j++)
-				{
-					fout << "d" << i << "W" << Wtable[j] << ", ";
-				}
+				fout << "d" << i << "W" << ", ";
 			}
 			
 			fout << endl;
@@ -203,39 +185,40 @@ void recursion(	int Dv, int Nv, double mv,
 		for (gen=0; gen < tv; gen++)
 		{
 			
-			for (i = 0; i < Dv; i++)  // for each deme
-				{
-					nb = i * Nv;
-					Wmax[i] = 0;
-				
-					for (j = 0; j < Nv; j++)  // for each individual
-					{
-						nb2 = 2 * (nb + j);
-				  
-						// determine hybrid index and heterozygosity
-						tmp1 = (pop[nb2].sel ^ pop[nb2+1].sel);
-						p12 = tmp1.count(); 
-						//p12 /= lv; //creates a vector with a 0 for homozygote and 1 for heterozygote sites
-						tmp2 = (pop[nb2].sel & pop[nb2+1].sel);
-						p2 = tmp2.count();
-						//p2 /= lv;
-						//HI = p2 + p12/2.0; 
-							
-						// fitness
-						indexW = getfitness_index(p2, p12, lv);	
-						w = Wtable[indexW];
-						
-						
-						Wtot[nb2/2] = w;
-							
-						if (gen == tv_1)
-							HIend[nb2/2] = HI;
 
-						if (Wmax[i] < w)
-							Wmax[i] = w;
-						
+			for (i = 0; i < Dv; i++)  // for each deme
+			{
+			    nb = i * Nv;
+			    Wmax[i] = 0;
+
+			    for (j = 0; j < Nv; j++)  // for each individual
+			    {
+					nb2 = 2 * (nb + j);
+					sz2 = 0;
+
+					for (k = 0; k < nv; k++)  // phenotypes of the individual
+					{
+						d = 0;
+						for (loc = 0; loc < lv; loc++)
+						{
+							if (pop[nb2].sel[loc] == 1)
+								d += mutations[lv * k + loc];
+							if(pop[nb2+1].sel[loc] == 1)
+								d+= mutations[lv *k + loc];
+						}
+			
+						sz2 += d * d; // "sz2" is the square of the distance to the optimum
 					}
-				}
+
+				// fitness:
+
+					w = getfitness(d, kd2, av);
+					Wtot[nb2/2] = w;
+
+					if (Wmax[i] < w)
+						Wmax[i] = w;
+			    }
+			}
 			
 			// sampling the next generation:
 			
@@ -385,21 +368,14 @@ void recursion(	int Dv, int Nv, double mv,
 					// Number of individuals with given fitness value per deme
 					for (i = 0; i < Dv; i++)
 					{
+						wbar = 0;
 					
-						for ( j =0; j < nbW; j++)
+						for ( j =0; j < Nv; j++)
 						{	
-							int histW = 0;
-
-							for ( ind = 0; ind < Nv; ind++)
-							{
-								if ( Wtot[ind + i * Nv] == Wtable[j])
-								{
-									histW += 1;
-								}
-							} 	
-							
-							fout << histW << ",";
+							wbar += Wtot[j];				
 						}	
+						wbar /= Nv;
+						fout << wbar << ",";
 					}
 					fout << endl;
 				}
